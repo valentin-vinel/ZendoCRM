466349f309b47550b20e0a46b38831ed50d5c6cf
valentin-vinel <valentinvnl84@gmail.com>
2025-09-12 19:05:10 +0200

    Mise en place serveur Express

---
+++ back/index.js
@@ -0,0 +1,30 @@
+import "dotenv/config";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { router } from "./app/routes/router.js";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const corsOptions = {
+	origin: [
+		`http://localhost:3000`
+	],
+	methods: ["GET", "POST", "PATCH", "DELETE"],
+	credentials: true
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+	res.send("Welcome to the ZendoCRM API!");
+});
+
+app.use(router);
+
+app.listen(port, () => {
+	console.log(`App listening on port ${port}`);
+});
