71a188d18b63d6a00764f74d7da2dd2bc54fd971
valentin-vinel <valentinvnl84@gmail.com>
2025-10-06 21:32:38 +0200

    front

--- front/src/app/crm/dashboard/page.tsx
+++ front/src/app/crm/dashboard/page.tsx
@@ -12,21 +12,33 @@ export default function Dashboard() {
                     <Header />
                     <div className="main p-6 mx-auto grid grid-cols-3 gap-4">
                         <div className="text-white flex gap-14 col-span-3">
-                            <div className="w-[230px] p-4 rounded-xl bg-black flex text-center">
-                                <p>Prospects <span className="font-bold">14</span></p>
+                            <div className="w-[230px] p-4 rounded-xl bg-black ">
+                                <p className="flex justify-between">
+                                    <span>Prospects</span>
+                                    <span className="font-bold">14</span>
+                                </p>
                             </div>
-                            <div className="w-[230px] p-4 rounded-xl bg-black flex text-center">
-                                <p>Clients <span className="font-bold">26</span></p>
+                            <div className="w-[230px] p-4 rounded-xl bg-black">
+                                <p className="flex justify-between">
+                                    <span>Clients</span>
+                                    <span className="font-bold">26</span>
+                                </p>
                             </div>
-                            <div className="w-[230px] p-4 rounded-xl bg-black flex text-center">
-                                <p>Opportunités <span className="font-bold">7</span></p>
+                            <div className="w-[230px] p-4 rounded-xl bg-black">
+                                <p className="flex justify-between">
+                                    <span>Opportunités</span> 
+                                    <span className="font-bold">7</span>
+                                </p>
                             </div>
-                            <div className="w-[230px] p-4 rounded-xl bg-black flex text-center">
-                                <p>CA <span className="font-bold">2753e</span></p>
+                            <div className="w-[230px] p-4 rounded-xl bg-black">
+                                <p className="flex justify-between">
+                                    <span>CA</span> 
+                                    <span className="font-bold">€ 2753</span>
+                                </p>
                             </div>
                         </div>
 
-                        <article className=" text-white bg-black h-[300px] rounded-xl p-4">
+                        <article className=" text-white bg-black h-[400px] rounded-xl p-4">
                             <h2 className="font-bold">Agenda du jour</h2>
                             <div className="py-4">
                                 <p><span className="text-gray-600">9:30 </span>- Prospection</p>
@@ -40,10 +52,10 @@ export default function Dashboard() {
                                 <p className="ml-12 text-gray-400">x100</p>
                             </div>
                         </article>
-                        <article className="w-full text-white bg-black h-[300px] rounded-xl p-4 col-span-2">
+                        <article className="w-full text-white bg-black h-[400px] rounded-xl p-4 col-span-2">
                             <h2 className="font-bold">Affaires en cours</h2>
                         </article>
-                        <article className="w-full text-white bg-black h-[220px] rounded-xl p-4 col-span-3">
+                        <article className="w-full text-white bg-black h-[300px] rounded-xl p-4 col-span-3">
                             <h2 className="font-bold">Contacts récents</h2>
                         </article>
                     </div>
